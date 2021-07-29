642537d93b5ad8c8f3d862728ab8fa40e3ed6274
vasia18 <studiovasia18@gmail.com>
2021-07-30 01:24:21 +0300

    Тестовый проект_3

---
+++ app/src/main/java/arkadii/ivanov/myapplication_test_1/ui/main/MainViewFragmentTwo.java
@@ -0,0 +1,4 @@
+package arkadii.ivanov.myapplication_test_1.ui.main;
+
+public class MainViewFragmentTwo {
+}
---
+++ app/src/main/java/arkadii/ivanov/myapplication_test_1/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package arkadii.ivanov.myapplication_test_1.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+	// TODO: Implement the ViewModel
+}
